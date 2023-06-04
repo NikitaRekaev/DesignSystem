71f270d8c4aa42e926630d3930c5890e6a22121a
NikitaRekaev <nvvvvk@gmail.com>
2023-06-04 18:46:26 +0400

    feat: add demo project

---
+++ Demo/SPMModulesTest/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//  SceneDelegate.swift
+//  Nikita Rekaev 30.05.2023
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return
+        }
+
+        let controller = WithImageViewController()
+        let navigationController = UINavigationController(rootViewController: controller)
+
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+}
+
